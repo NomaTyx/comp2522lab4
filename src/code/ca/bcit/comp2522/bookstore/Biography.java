59f3b13c18e0156ea175d06631164ef82b15d1e6
tanmayvee <mtan93@my.bcit.ca>
2026-02-08 01:12:55 -0800

    wrote Biography class. need to adust autobiography still

--- src/code/ca/bcit/comp2522/bookstore/Biography.java
+++ src/code/ca/bcit/comp2522/bookstore/Biography.java
@@ -10,20 +10,57 @@ package ca.bcit.comp2522.bookstore;
  */
 public class Biography extends Book
 {
+    private final Person subject;
+
     /**
      * Constructs a Biography.
      *
      * @param title  the title
      * @param author the author
      */
-    public Biography(final String title, final int yearPublished, final Author author)
+    public Biography(final String title, final int yearPublished, final Author author, final Person subject)
     {
         super(title, yearPublished, author);
+        if(subject == null)
+        {
+            throw new IllegalArgumentException("Subject cannot be null.");
+        }
+
+        this.subject = subject;
+
+    }
+    public Person getSubject()
+    {
+        return subject;
     }
 
     @Override
-    public void display() {
+    public void display()
+    {
+        System.out.print("Biography ");
+        super.display();
+        System.out.print("Subject is ");
+        subject.display();
+    }
 
+    /**
+     * Two biographies are equal if they are about the same subject.
+     */
+    @Override
+    public boolean equals(final Object obj)
+    {
+        if(this == obj)
+        {
+            return true;
+        }
+
+        if(!(obj instanceof Biography))
+        {
+            return false;
+        }
+
+        final Biography other = (Biography) obj;
+        return subject.equals(other.subject);
     }
 }
 
